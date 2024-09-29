1aab43599ef46e31db6f4bd5dff975ca744fa937
aaryan-verma <aryanverma.av2000@gmail.com>
2024-09-30 03:51:34 +0530

    fix: changed root api route for deployment

--- frontend/src/utils/axiosInstance.ts
+++ frontend/src/utils/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:5000",
-  baseURL: "http://13.202.230.102",
+  baseURL: "http://13.202.230.102/api",
   headers: {
     "Content-Type": "application/json",
   },
