0a7db24a23179cc5240cac638182a91ee65d4b7f
aaryan-verma <aryanverma.av2000@gmail.com>
2024-09-29 22:34:12 +0530

    chore: removed commented code

--- frontend/src/utils/axiosInstance.ts
+++ frontend/src/utils/axiosInstance.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://moviecritic-7u9x.onrender.com/",
   baseURL: "http://localhost:5000",
   headers: {
     "Content-Type": "application/json",
